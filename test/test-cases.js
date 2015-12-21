641ffcd4403e54a2379ac01173c0cee7359f3e55
Jérôme Desboeufs <jerome.desboeufs@gmail.com>
2015-12-21 17:06:55 +0100

    Add Rennes Métropole test case

--- test/test-cases.js
+++ test/test-cases.js
@@ -9,13 +9,18 @@ const Parser = require('../lib/parser');
 const definition = jsYaml.safeLoad(fs.readFileSync(__dirname + '/../schema/md-metadata.yml'));
 
 describe('Real test cases', function () {
-    it('geopicardie-tache-urbaine should be parsed successfully', function (done) {
-        const parser = new Parser('MD_Metadata', definition);
-        const xml = fs.createReadStream(__dirname + '/test-cases/geopicardie-tache-urbaine.xml');
-        const expectedResult = require('./test-cases/geopicardie-tache-urbaine.json');
-        xml.pipe(parser).once('result', result => {
-            expect(result).to.eql(expectedResult);
-            done();
+    [
+        'geopicardie-tache-urbaine',
+        'rennes-metropole-referentiel-adresse',
+    ].forEach(testCase => {
+        it(testCase + ' should be parsed successfully', function (done) {
+            const parser = new Parser('MD_Metadata', definition);
+            const xml = fs.createReadStream(__dirname + '/test-cases/' + testCase + '.xml');
+            const expectedResult = require('./test-cases/' + testCase + '.json');
+            xml.pipe(parser).once('result', result => {
+                expect(result).to.eql(expectedResult);
+                done();
+            });
         });
-    });
+    })
 });
