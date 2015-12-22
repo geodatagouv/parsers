5117eb7aa584667cbd90b40a643d0f366e38f1d3
Jérôme Desboeufs <jerome.desboeufs@gmail.com>
2015-12-22 18:19:19 +0100

    Add simple child, collection child and attributes support

--- test/test-cases.js
+++ test/test-cases.js
@@ -7,23 +7,32 @@ const fs = require('fs');
 const Parser = require('../lib/parser');
 
 const definition = jsYaml.safeLoad(fs.readFileSync(__dirname + '/../schema/md-metadata.yml'));
+Object.assign(definition, jsYaml.safeLoad(fs.readFileSync(__dirname + '/../schema/csw.yml')));
 
-describe('Real test cases', function () {
+function createTestCase(name, schema) {
+    it(name + ' should be parsed successfully', function (done) {
+        const parser = new Parser(schema, definition);
+        const xml = fs.createReadStream(__dirname + '/test-cases/' + name + '.xml');
+        const expectedResult = require('./test-cases/' + name + '.json');
+        xml.pipe(parser).once('result', result => {
+            expect(result).to.eql(expectedResult);
+            done();
+        });
+    });
+}
+
+describe('Real test cases: MD_Metadata', function () {
     [
         'geopicardie-tache-urbaine',
         'rennes-metropole-referentiel-adresse',
         'ifremer-multilang',
         'siglr-malformed',
         'geobretagne-service-coupled',
-    ].forEach(testCase => {
-        it(testCase + ' should be parsed successfully', function (done) {
-            const parser = new Parser('MD_Metadata', definition);
-            const xml = fs.createReadStream(__dirname + '/test-cases/' + testCase + '.xml');
-            const expectedResult = require('./test-cases/' + testCase + '.json');
-            xml.pipe(parser).once('result', result => {
-                expect(result).to.eql(expectedResult);
-                done();
-            });
-        });
-    })
+    ].forEach(testCase => createTestCase(testCase, 'MD_Metadata'));
+});
+
+describe('Real test cases: GetRecordsResponse', function () {
+    [
+        'csw-getrecordsresponse',
+    ].forEach(testCase => createTestCase(testCase, 'GetRecordsResponse'));
 });
